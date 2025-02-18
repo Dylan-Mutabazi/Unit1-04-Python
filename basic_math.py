dca4953ff327ec97420991522662a7fff44d1a23
Dylan Mutabazi <dylan.mutabazi@stu.ocsb.ca>
2025-02-18 23:34:54 +0000

    Added it

---
+++ basic_math.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Created by Dylan Mutabazi
+# Date :2025-02-14
+# This program does basic math
+
+
+
+def main():
+    print("3 + 4 = {}".format(3 + 4))
+    print("13 - 4 = {}".format(13 - 4))
+    print("7 x 4 = {}".format(7 * 4))
+    print("24 / 4 = {}".format(24 / 4))
+    print("4 ^ 2 = {}".format(4**2))
+    print("9 √ 0.5 = {}".format(9**0.5))
+
+
+if __name__ == "__main__":
+    main()
